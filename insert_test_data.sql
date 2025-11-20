75d9e67e15774210b41d4907d9d6a2a53e7ff16b
schou008​ <saqibchy9@gmail.com>
2025-11-20 15:54:11 +0000

    Updated code

--- insert_test_data.sql
+++ insert_test_data.sql
@@ -4,6 +4,7 @@ USE berties_books;
 
 INSERT INTO books (name, price)VALUES('Brighton Rock', 20.25),('Brave New World', 25.00), ('Animal Farm', 12.99) ;
 
+# Password is 'smiths', stored as a bcrypt hash
 INSERT INTO users (username, firstName, lastName, email, hashedPassword)
 VALUES
-('gold', 'Gold', 'User', 'gold@example.com', '$2b$10$8gW7xIuU2Ff2KgOZkB4hXeRcGu8pFxe3xe4Jjq3x1CFiWpkEqnc8.');
+('gold', 'Gold', 'User', 'gold@example.com', '$2b$10$ElB7hpK/3pzOOFRdNzBEMOqzrdtN3dDl3l6h93FQIQWIk8vsfGH/q');
